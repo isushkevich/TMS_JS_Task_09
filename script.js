81b923abbd6a5de9fe529b1ca53a3caf62e5b2f7
isushkevich <ivan.sushik@gmail.com>
2020-03-11 23:13:31 +0300

    add a car obj in another way

--- script.js
+++ script.js
@@ -25,16 +25,21 @@ function Car(brand, model, type, cc) {
 }
 
 
-let car1 = new Car("Honda", "Civic", "Sedan", 2000); //
-let car2 = new Car("Fiat", "Panda", "Hatchback", 1600); //
+let car1 = new Car("Honda", "Civic", "Sedan", 2000);
+let car2 = new Car("Fiat", "Panda", "Hatchback", 1600);
 let car3 = new Car("Subaru", "Impreza", "Sedan", 3500);
 let car4 = new Car("Ferrari", "SP38", "Supercar", 3900);
-let car5 = new Car("Volkswagen", "Golf", "Hatchback", 1800); //
-let car6 = new Car("Honda", "S2000", "Roadster", 2200); //
+let car5 = new Car("Volkswagen", "Golf", "Hatchback", 1800);
+let car6 = new Car("Honda", "S2000", "Roadster", 2200);
 let car7 = new Car("BMW", "M5", "Sedan", 4400);
 let car8 = new Car("Volkswagen", "Touareg", "SUV", 4130);
-let car9 = new Car("Ford", "Focus", "Hatchback", 2000); //
-let car10 = new Car("Bugatti", "Chiron", "Supercar", 6000);
+let car9 = new Car("Ford", "Focus", "Hatchback", 2000);
+let car10 = new Car();
+
+car10.setBrand("Bugatti");
+car10.setModel("Chiron");
+car10.setType("Supercar");
+car10.setCc(6000);
 
 
 let cars = [car1, car2, car3, car4, car5, car6, car7, car8, car9, car10];
